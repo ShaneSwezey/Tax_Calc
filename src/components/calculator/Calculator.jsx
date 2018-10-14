cc3b791e2462b819eaf1d80a7cacd42fff314ba9
Shane Swezey <shanedswezey@gmail.com>
2018-10-14 14:53:49 -0700

    Changed from class component to function component

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 import Form from './Form';
 import './Calculator.css';
 
-class Calculator extends Component {
-    render() {
-        return (
-            <article id="Calc">
-                <section id="CalcSection">
-                    <Form />
-                </section>
-            </article>
-        );
-    }
-}
+const Calculator = props => (
+    <article id="Calc">
+        <section id="CalcSection">
+            <Form />
+        </section>
+    </article>
+)
 
 export default Calculator;
