43615d915ea742821e8f8ac1708c4594c2bc3730
Shane Swezey <shanedswezey@gmail.com>
2018-10-15 18:35:14 -0700

    Created rootReducer and added to store

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { Provider} from 'react-redux';
+import rootReducer from './reducers/rootReducer';
 
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    filingStatus: 'single',
+    income: 0,
+    filingYear: 0,
+    incomeYears: []
+}
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer;
