fa918723d6e0d21c51536ac1b5a6125964ae7661
Shane Swezey <shanedswezey@gmail.com>
2018-10-15 18:29:47 -0700

    Added store and Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore();
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
