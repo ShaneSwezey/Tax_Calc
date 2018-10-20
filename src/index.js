31a6d165d773581a42737f2f8517c3745911cf48
Shane Swezey <shanedswezey@gmail.com>
2018-10-20 16:40:21 -0700

    Applied thunk to middleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 import yearReducer from './reducers/yearReducer';
 
 
-const store = createStore(yearReducer);
+const store = createStore(yearReducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
