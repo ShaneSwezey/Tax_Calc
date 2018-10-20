046a09cd2436311b2565e3c50ea7d20bb560abdc
Shane Swezey <shanedswezey@gmail.com>
2018-10-19 19:04:14 -0700

    Added yearReaducer.js

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,11 @@
+
 const initState = {
     filingStatus: 'single',
     income: 0,
     filingYear: 0,
-    incomeYears: []
+    incomeYears: [],
+    loading: false,
+    error: null
 }
 
 const rootReducer = (state = initState, action) => {
---
+++ src/reducers/yearReducer.js
@@ -0,0 +1,29 @@
+import {
+    FETCH_YEARS_BEGIN,
+    FETCH_YEARS_SUCCESS,
+    FETCH_YEARS_FAILURE
+} from '../actions/yearActions';
+
+export default function yearReducer(state = initialState, action) {
+    switch(action.type) {
+        case FETCH_YEARS_BEGIN:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
+        case FETCH_YEARS_SUCCESS:
+            return {
+                ...state,
+                incomeYears: action.payload.years
+            };
+        case FETCH_YEARS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload.error,
+            };
+        default:
+            return state;
+    }
+}
