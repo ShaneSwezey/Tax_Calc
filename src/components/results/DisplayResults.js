12c95f05504b5fb699415a9e49605326e9f8dd44
Shane Swezey <shanedswezey@gmail.com>
2018-10-31 17:28:49 -0700

    Added missing percentage signs

--- src/components/results/DisplayResults.js
+++ src/components/results/DisplayResults.js
@@ -4,9 +4,9 @@ const DisplayResults = ({data: { year, taxBracket, taxAmount, percOfInc, rates }
     <section>
         <h1>Results</h1>
         <div>Year: {year}</div>
-        <div>Tax Bracket: {taxBracket}</div>
+        <div>Tax Bracket: {taxBracket}%</div>
         <div>Amount Taxed: ${taxAmount}</div>
-        <div>Percentage of Income: {percOfInc}</div>
+        <div>Percentage of Income: {percOfInc}%</div>
     </section>
 );
 
