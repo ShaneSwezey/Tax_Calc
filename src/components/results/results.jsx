216c3f888cc42c19c42f701fb97710d00e719d55
Shane Swezey <shanedswezey@gmail.com>
2018-11-08 16:50:34 -0800

    Chart options folder

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -4,7 +4,6 @@ import Result from './Result.jsx';
 import PercentResult from './PercentResult.jsx';
 import DollarResult from './DollarResult';
 import Chart from '../charts/Chart.jsx';
-import Highcharts from 'highcharts';
 
 class Results extends Component {
     render() {
@@ -15,59 +14,11 @@ class Results extends Component {
             taxBracket: this.props.taxBracket,
             taxAmount: this.props.taxAmount.toLocaleString('en'), // Number formatting
             percOfInc: this.props.percOfInc,
-            socialSecurityTax: this.props.socialSecurityTax.toLocaleString('en'),
-            medicareTax: this.props.medicareTax.toLocaleString('en'),
+            socialSecurityTax: this.props.socialSecurityTax.toLocaleString('en'), // Number formatting
+            medicareTax: this.props.medicareTax.toLocaleString('en'), // Number formatting
             rate: this.props.rates
         };
         
-        const options = {
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Income Tax Breakdown'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                        style: {
-                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                        }
-                    }
-                }
-            },
-            credits: {
-                enabled: false,
-            },
-            series: [{
-                name: 'Tax',
-                colorByPoint: true,
-                data: [{
-                    name: 'Income Tax',
-                    y: this.props.taxAmount,
-                    sliced: true,
-                    selected: true
-                }, {
-                    name: 'Social Security Tax',
-                    y: this.props.socialSecurityTax
-                }, {
-                    name: 'Medicare Tax',
-                    y: this.props.medicareTax
-                }
-                ]
-            }]
-        };
-
         return (
             <article id="results">
                 <section> 
