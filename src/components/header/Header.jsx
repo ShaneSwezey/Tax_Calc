eec5c5bff014b3974496160b97b813a2c4d7ec9d
Shane Swezey <shanedswezey@gmail.com>
2018-11-11 17:37:34 -0800

    Code clean up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => (
-            <header className="Header">
-                <nav>
-                    <ul>
-                        <li><a href="#About">The Federal Income Tax</a></li>
-                        <li><a href="#Calc">Federal Income Tax Bracket Calculator</a></li>
-                    </ul>
-                </nav>
-            </header>
+    <header className="Header">
+        <nav>
+            <ul>
+                <li><a href="#About">The Federal Income Tax</a></li>
+                <li><a href="#Calc">Federal Income Tax Bracket Calculator</a></li>
+            </ul>
+        </nav>
+    </header>
 );
 
 export default Header;
